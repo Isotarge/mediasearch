4017807949d14659bd29005b3977d4562bf0b515
Isotarge <isaacjmiell@gmail.com>
2021-08-26 14:34:34 +0930

    Test

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <Header msg="Media Search" />
+    <Header msg="Media Search (Vite Test)" />
     <div id="panel-container">
       <MoodSliders :sliders="sliders" @signal-recompute-recommendations="computeRecommendations" />
       <RecommendationPanel
